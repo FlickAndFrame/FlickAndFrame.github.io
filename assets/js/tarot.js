7bf3a53b3ec2f8dbf11d71c0446349243e7d1320
FlickAndFrame <vittorio.c@live.com>
2024-09-30 14:28:41 +0200

    Update tarot.js

--- assets/js/tarot.js
+++ assets/js/tarot.js
@@ -334,7 +334,7 @@ document.addEventListener('DOMContentLoaded', function() {
             document.getElementById('totalCount').textContent = data.totalCount;
 
             // Extract the suggestion from the response
-            const suggestion = data.suggestion;
+            const suggestion = JSON.parse(data.suggestion);
 			
 			// Extract the body from the response
             //const suggestion = JSON.parse(data.body);
